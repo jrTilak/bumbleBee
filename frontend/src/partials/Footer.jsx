5e8ae37adff6984ef10078db4393c12b5a329cb6
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-18 08:06:46 +0545

    Remove social media links from Footer component

--- frontend/src/partials/Footer.jsx
+++ frontend/src/partials/Footer.jsx
@@ -8,24 +8,9 @@ function Footer() {
         <div className="py-4 border-t border-gray-200 md:flex md:items-center md:justify-between md:py-8">
           {/* Social links */}
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-            <li>
-              <Link
-                to="#"
-                className="flex items-center justify-center text-gray-600 transition duration-150 ease-in-out bg-white rounded-full shadow hover:text-gray-900 hover:bg-white-100"
-                aria-label="Twitter"
-              >
-                <svg
-                  className="w-8 h-8 fill-current"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
-                </svg>
-              </Link>
-            </li>
             <li className="ml-4">
               <Link
-                to="#"
+                to="https://github.com/jrTilak"
                 className="flex items-center justify-center text-gray-600 transition duration-150 ease-in-out bg-white rounded-full shadow hover:text-gray-900 hover:bg-white-100"
                 aria-label="Github"
               >
@@ -38,21 +23,6 @@ function Footer() {
                 </svg>
               </Link>
             </li>
-            <li className="ml-4">
-              <Link
-                to="#"
-                className="flex items-center justify-center text-gray-600 transition duration-150 ease-in-out bg-white rounded-full shadow hover:text-gray-900 hover:bg-white-100"
-                aria-label="Facebook"
-              >
-                <svg
-                  className="w-8 h-8 fill-current"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
-                </svg>
-              </Link>
-            </li>
           </ul>
 
           {/* Copyrights note */}
@@ -60,9 +30,9 @@ function Footer() {
             Made by{" "}
             <a
               className="text-blue-600 hover:underline"
-              href="https://cruip.com/"
+              href="https://thapatilak.com.np/"
             >
-              Cruip
+              jrTilak
             </a>
             . All rights reserved.
           </div>
